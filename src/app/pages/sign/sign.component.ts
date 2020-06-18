d0737cff2cff1648b844341abb9f0c4e4bc21350
Burak Duman <b.duman@amatis.nl>
2020-06-19 00:47:51 +0300

    Sign page updated

--- src/app/pages/sign/sign.component.ts
+++ src/app/pages/sign/sign.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign.component.css']
 })
 export class SignComponent implements OnInit {
-
+  formModel = {
+    profile : null,
+    receipt : null,
+    name: null,
+    identityNumber: null,
+    motherName: null,
+    fatherName: null,
+    socialAccount: null,
+    bloodGroup: null,
+    telNo: null,
+    identitySerialNumber: null,
+    job: null,
+    expireDate: null,
+    homeAddress: null,
+    jobAddress: null,
+    graduationSchool: null,
+    education: null,
+    city: null,
+    district: null,
+    village: null,
+    volumeNo: null,
+    familyNo: null,
+    itemNo: null
+  };
   constructor() { }
 
   ngOnInit() {
   }
+
+  sendForm(){
+    console.log("model",this.formModel);
+  }
+  addFile(input, key){
+    const file: File = input.files[0];
+     this.getBase64(file,key);
+    
+  }
+
+  getBase64(file,key) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload =() => {      
+      this.formModel[key] =  reader.result;
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
 
 }
